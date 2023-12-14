9f8a2a4a1dfce8437790531d6bde74e64726f9ff
MNZhavoronkov <zhavkk@gmail.com>
2023-12-14 20:20:37 +0300

    2

--- realization2.c
+++ realization2.c
@@ -1,15 +1,89 @@
 #include <stdio.h>
 #include <math.h>
 #include "funcs.h"
+#include <stdlib.h>
 
 
+int PrimeCount(int A,int B){ // решето
+    int N=B-A+1;
+    int* massiv = (int*)malloc(N * sizeof(int));
+    if (massiv == NULL) {
+        printf("Memory allocation failed.\n");
+        return -1;
 
-int PrimeCount(int A,int B){
+    }   
+    int counter=0;
+    for(int i=2;i*i<N;++i){
+        if(massiv[i]==0){
+            for(int k=i*i;k<N;k+=i){
+                massiv[k]=1;
+            }
+            
+        }
+    }
 
+    for(int i=2;i<N;++i){
+        if(massiv[i]==0){
+            counter+=1;
+        }
+
+    }
+    return counter;
 }
 
 
 
 
 
-int *sort(int *array,int size);
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int partition(int *array, int low, int high) {
+    int pivot = array[low];
+    int left = low + 1;
+    int right = high;
+
+    while (true) {
+        while (left <= right && array[left] < pivot) {
+            ++left;
+        }
+
+        while (right >= left && array[right] > pivot) {
+            --right;
+        }
+
+        if (left <= right) {
+            swap(array[left], array[right]);
+        } else {
+            break;
+        }
+    }
+
+    swap(array[low], array[right]);
+    return right;
+}
+
+void quicksort(int *array, int low, int high) {
+    if (low < high) {
+        int pivotIndex = partition(array, low, high);
+        quicksort(array, low, pivotIndex - 1);
+        quicksort(array, pivotIndex + 1, high);
+    }
+}
+
+int *sort(int *array, int size) {
+    quicksort(array, 0, size - 1);
+    return array;
+}
+
+
+
+
+
+int main(int argc,char *argv[]){
+
+    printf("%d",PrimeCount(1,100));
+}
