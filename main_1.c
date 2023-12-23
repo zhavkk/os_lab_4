c4d6f4ab46bdb4ccf4387b1bc9440ae3e2b91855
MNZhavoronkov <zhavkk@gmail.com>
2023-12-23 18:59:50 +0300

    fix

--- main_1.c
+++ main_1.c
@@ -2,57 +2,67 @@
 #include <stdlib.h>
 #include "funcs.h"
 
-int main() {
-    printf("1 arg1 arg2 - PrimeCount function\n2 - Sort function\n3 exit\n");
-    int com;
-    int a, b;
-    int size;
+int main(){
+    
+    printf("1 arg1 arg2 - PrimeCount function\n");
+    printf("2 arg1 your array - Sort function\n");
+    printf("3 - exit\n");
+
+    int command;
     int *array = NULL;
 
-    printf("Input the command: ");
-    scanf("%d", &com);
+    printf("Input command: ");
+    scanf("%d",&command);
 
-    while (com != 3) {
-        switch (com) {
-            case 1:
-                printf("Input a and b: ");
-                scanf("%d %d", &a, &b);
-                printf("PrimeCount(%d, %d) = %d\n", a, b, PrimeCount(a, b));
+    while(command != 3){
+        switch (command){
+        case 1:
 
-                printf("Input the command: ");
-                scanf("%d", &com);
-                break;
-            case 2:
-                printf("Input the size of your array: ");
-                scanf("%d", &size);
-                array = (int *)malloc(size * sizeof(int));
+            int x,y;
 
-                printf("Input your array: ");
-                for (int i = 0; i < size; ++i) {
-                    scanf("%d", &array[i]);
-                }
+            printf("\nInput args: ");
+            scanf("%d %d",&x,&y);
+            printf("-----------------\n");
 
-                int *sortedArray = sort(array, size);
+            printf("PrimeCount(%d, %d) = %d\n",x,y,PrimeCount(x,y));
+            printf("-----------------\n");
 
-                printf("Sorted array: ");
-                for (int i = 0; i < size; ++i) {
-                    printf("%d ", sortedArray[i]);
-                }
-                printf("\n");
+            printf("\nInput command: ");
+            scanf("%d",&command);
 
-                free(sortedArray);
+            break;
+        case 2:
 
-                printf("Input the command: ");
-                scanf("%d", &com);
-                break;
-            case 3:
-                break;
-            default:
-                printf("Incorrect input\n");
-                exit(1);
-                break;
+            int size;
+            printf("Input the size of your array: ");
+            scanf("%d", &size);
+            array = (int *)malloc(size * sizeof(int));
+
+            printf("Input your array: ");
+            for (int i = 0; i < size; ++i) {
+                scanf("%d", &array[i]);
+            }
+
+            int *sortedArray = sort(array, size);
+
+            printf("Sorted array: ");
+            for (int i = 0; i < size; ++i) {
+                printf("%d ", sortedArray[i]);
+            }
+            printf("\n");
+            free(sortedArray);
+            printf("\nInput command: ");
+            scanf("%d",&command);
+
+            break;
+
+        case 3:
+            break;
+
+        default:
+            printf("\nIncorrect Input\n");
+            exit(1);
+            break;
         }
     }
-
-    return 0;
 }
--- main_2.c
+++ main_2.c
@@ -1,90 +1,129 @@
 #include <stdio.h>
 #include <dlfcn.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main() {
-    int com, a, b, size;
+int main(){
+
+    printf("0 - Change realization\n");
+    printf("1 arg1 arg2 - PrimeCount function\n");
+    printf("2 arg1 your array - Sort function\n");
+    printf("3 - exit\n");
+
+    int flag_lib = 1;      // номер текущей библиотеки
+    void *library_handler; // прямой указатель на начало динамической библиотеки
     int *array = NULL;
-    void *library;
-    char name1[] = "PrimeCount";
-    char name2[] = "sort";
+    int size;
 
-    printf("0 - change realisation\n1 arg1 arg2 - PrimeCount function\n2 your array - Sort function\n3 exit\n");
-    printf("Input your command: ");
-    scanf("%d", &com);
+    library_handler = dlopen("librealization1.so",RTLD_LAZY);
 
-    library = dlopen("librealization1.so", RTLD_NOW);
+    if (!library_handler){
+        // если ошибка, то вывести ее на экран
+        fprintf(stderr,"dlopen() error: %s\n", dlerror());
+        exit(1); // в случае ошибки закончить работу программу
+    };
 
-    if (!library) {
-        fprintf(stderr, "dlopen error: %s\n", dlerror());
-        return 1;
-    }
+    int command;
+    printf("Input command: ");
+    scanf("%d",&command);
 
-    while (com != 3) {
-        switch (com) {
-            case 0:
-                dlclose(library);
-                if (library == 1) {
-                    library = dlopen("librealization1.so", RTLD_NOW);
-                } else {
-                    library = dlopen("librealization2.so", RTLD_NOW);
-                }
-                printf("Realisation was changed.\n");
-                printf("Input your command: ");
-                scanf("%d", &com);
-                break;
-            case 1:
-                printf("input a and b: ");
-                scanf("%d %d", &a, &b);
-                int (*func_primeCount)(int, int);
-                func_primeCount = (int (*)(int, int))dlsym(library, name1);
+    while(command != 3){
+        switch (command){
+        case 0:
+            
+            dlclose(library_handler); // закрываю текущую библиотеку
 
-                if (!func_primeCount) {
-                    fprintf(stderr, "PrimeCount error: %s\n", dlerror());
-                    return 1;
-                }
+            if(flag_lib == 1){
+                library_handler = dlopen("librealization2.so",RTLD_LAZY);
+                flag_lib = 2;
+            }
+            else{
+                library_handler = dlopen("librealization1.so",RTLD_LAZY);
+                flag_lib = 1;
+            }
 
-                printf("PrimeCount result: %d\n", func_primeCount(a, b));
-                printf("Input your command: ");
-                scanf("%d", &com);
-                break;
-            case 2:
-                printf("Input the size of your array: ");
-                scanf("%d", &size);
-                array = (int *)malloc(size * sizeof(int));
+            printf("\nREALIZATION CHANGED\n");
 
-                printf("Input your array: ");
-                for (int i = 0; i < size; ++i) {
-                    scanf("%d", &array[i]);
-                }
+            printf("Input command: ");
+            scanf("%d",&command);
 
-                void (*func_sort)(int *, int);
-                func_sort = (void (*)(int *, int))dlsym(library, name2);
+            break;
 
-                if (!func_sort) {
-                    fprintf(stderr, "sort function error: %s\n", dlerror());
-                    return 1;
-                }
+        case 1:
+            int x,y;
 
-                func_sort(array, size);
+            printf("\nInput args: ");
+            scanf("%d %d",&x,&y);
+            printf("-----------------\n");
 
-                printf("Sorted array: ");
-                for (int i = 0; i < size; ++i) {
-                    printf("%d ", array[i]);
-                }
-                printf("\n");
+            int (*func_PrimeCount)(int, int); // указатель на функцию
+            char name1[] = "PrimeCount";
 
-                free(array);
+            func_PrimeCount = dlsym(library_handler,name1);
 
-                printf("Input your command: ");
-                scanf("%d", &com);
-                break;
-            default:
-                printf("Incorrect input\n");
+            if (!func_PrimeCount){
+                fprintf(stderr,"dlopen() error: %s\n", dlerror());
+                exit(1);
+            };
+            
+            if(flag_lib == 1) printf("\nPrimeCount realization №1\n");
+            else printf("\nPrimeCount realization №2\n");
+
+            printf("PrimeCount(%d, %d) = %d\n",x,y,(*func_PrimeCount)(x, y));
+            printf("-----------------\n");
+
+            printf("\nInput command: ");
+            scanf("%d",&command);
+
+            break;
+
+        case 2:
+            printf("Input the size of your array: ");
+            scanf("%d", &size);
+            array = (int *)malloc(size * sizeof(int));
+
+            printf("Input your array: ");
+            for (int i = 0; i < size; ++i) {
+                scanf("%d", &array[i]);
+            }
+
+            void (*func_sort)(int *, int);
+            char name2[]="sort";
+
+            if(flag_lib == 1) printf("\nSort realization №1\n");
+            else printf("\nSort realization №2\n");
+
+            func_sort = (void (*)(int *, int))dlsym(library_handler, name2);
+
+            if (!func_sort) {
+                fprintf(stderr, "sort function error: %s\n", dlerror());
                 return 1;
+            }
+
+            func_sort(array, size);
+
+            printf("Sorted array: ");
+            for (int i = 0; i < size; ++i) {
+                printf("%d ", array[i]);
+            }
+            printf("\n");
+
+            free(array);
+            
+            printf("\nInput command: ");
+            scanf("%d",&command);
+
+            break;
+        case 3:
+            break;
+        default:
+
+            printf("\nIncorrect Input\n");
+        
+            exit(1);
+        
+            break;
         }
     }
-
-    dlclose(library);
-
-    return 0;
+    dlclose(library_handler);
 }
